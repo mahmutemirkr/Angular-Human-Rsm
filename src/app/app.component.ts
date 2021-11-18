a318b53f1e2a12a204f2c5ae88f3087fdcdc4fab
Emir <mahmutemirkr@gmail.com>
2021-11-19 02:52:23 +0300

    v6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Role } from './models/role.enum';
+import { User } from './models/user.model';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-human-rsm';
+
+  currentUser : User = new User;
+
+  constructor(private authenticationService: AuthenticationService, private router: Router){
+    this.authenticationService.currentUser.subscribe(data=>{
+      this.currentUser = data;
+    });
+  }
+
+  isAdmin(){
+    return this.currentUser?.role == Role.ADMIN;
+  }
+  
+  logOut(){
+    this.authenticationService.logOut();
+    this.router.navigate(['/login']);
+  }
+
 }
